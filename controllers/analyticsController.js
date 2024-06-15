3f16854e9426d43ee13ba3eee66cb54af6d705ee
Prabhat kumar <prabhatkumar944@gmail.com>
2024-06-15 09:08:29 +0530

    added health moniter by patient id

--- controllers/analyticsController.js
+++ controllers/analyticsController.js
@@ -34,21 +34,131 @@ exports.getAverageBloodPressure = async (req,res) => {
         }
 };
 
+exports.getAverageTemperature = async (req,res) => {
+    try {
+        const averageTemperature = await HealthData.aggregate([
+            {
+                $group:{
+                    _id:null,
+                    averageTemperature:{$avg:"$temperature"}
+                }
+            }
+        ]);
+
+        res.json(averageTemperature);
+    }catch (err) {
+        res.status(500).send('Something went wrong on server');
+        }
+};
+
+
 exports.getDailyHealthStats = async (req, res) => {
     try{
        const dailyStats = await HealthData.aggregate([
         {
             $group:{
-                _id:{$dateToString:{format:"%Y=%m-%d", date:"$timestamp"}},
+                _id: { $dateToString: { format: "%Y-%m-%d", date: "$timeStamp" } },
                 averageHeartRate:{$avg:"$heartRate"},
-                averageTemperature: {$avg:"$temperature"},
                 averageBloodPressure:{$avg:"$bloodPressure"},
+                averageTemperature:{$avg:"$temperature"},
                 count:{$sum:1}
             }
+        },
+        {
+          $sort: { _id: 1 } // Sort by date
         }
        ]);
        res.json(dailyStats);
     }catch(err){
         res.status(500).send('Something went wrong on server');
     }
+};
+
+exports.getAverageHeartRateByPatientId = async (req, res) => {
+    try {
+        const { patientId } = req.params;
+        const avgHeartRate = await HealthData.aggregate([
+            {
+                $match: { patientId }
+            },
+            {
+                $group:{
+                    _id:null,
+                    averageHeartRate:{$avg:"$heartRate"}
+                }
+            }
+        ]);
+
+        res.json(avgHeartRate);
+    }catch (err) {
+        res.status(500).send('Something went wrong on server');
+        }
+};
+
+exports.getAverageBloodPressureByPatientId = async (req,res) => {
+    try {
+        const { patientId } = req.params;
+        const averageBloodPressure = await HealthData.aggregate([
+            {
+                $match: { patientId }
+            },
+            {
+                $group:{
+                    _id:null,
+                    averageBloodPressure:{$avg:"$bloodPressure"}
+                }
+            }
+        ]);
+
+        res.json(averageBloodPressure);
+    }catch (err) {
+        res.status(500).send('Something went wrong on server');
+        }
+};
+
+exports.getAverageTemperatureByPatientId = async (req,res) => {
+    try {
+        const { patientId } = req.params;
+        const averageTemperature = await HealthData.aggregate([
+            {
+                $match: { patientId }
+            },
+            {
+                $group:{
+                    _id:null,
+                    averageTemperature:{$avg:"$temperature"}
+                }
+            }
+        ]);
+
+        res.json(averageTemperature);
+    }catch (err) {
+        res.status(500).send('Something went wrong on server');
+        }
+};
+
+exports.getDailyHealthStatsByPatientId = async (req,res) => {
+    try {
+        const { patientId } = req.params;
+        const dailyStats = await HealthData.aggregate([
+          {
+            $match: { patientId }
+          },
+          {
+            $group: {
+              _id: { $dateToString: { format: "%Y-%m-%d", date: "$timeStamp" } },
+              averageHeartRate:{$avg:"$heartRate"},
+              averageBloodPressure:{$avg:"$bloodPressure"},
+              averageTemperature:{$avg:"$temperature"},
+              count:{$sum:1}
+            }
+          },
+          {
+            $sort: { _id: 1 }
+          }
+        ]);
+        res.json(dailyStats);
+      } catch (err) {
+        res.status(500).send('Something went wrong on the server');
+      }
 };
--- routes/analyticsRoutes.js
+++ routes/analyticsRoutes.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const { getAverageHeartRate, getAverageBloodPressure, getDailyHealthStats } = require('../controllers/analyticsController');
+const { getAverageHeartRate, getAverageHeartRateByPatientId, getAverageBloodPressure, getAverageBloodPressureByPatientId, getAverageTemperature, getAverageTemperatureByPatientId, getDailyHealthStats, getDailyHealthStatsByPatientId } = require('../controllers/analyticsController');
 const auth = require('../middleware/authMiddleware');
 
 
 router.get('/average-heart-rate', auth, getAverageHeartRate);
+router.get('/average-heart-rate/:patientId', auth, getAverageHeartRateByPatientId);
 router.get('/average-blood-pressure', auth, getAverageBloodPressure);
+router.get('/average-blood-pressure/:patientId', auth, getAverageBloodPressureByPatientId);
+router.get('/average-temperature', auth, getAverageTemperature);
+router.get('/average-temperature/:patientId', auth, getAverageTemperatureByPatientId);
 router.get('/daily-stats', auth, getDailyHealthStats);
+router.get('/daily-stats/:patientId', auth, getDailyHealthStatsByPatientId);
 
 module.exports = router;
